7308599ad2f2c175bd13f75857b3205ce710df06
Robbi Garza <xaleth09@gmail.com>
2025-04-08 19:35:55 -0700

    refactor: tweak Card component

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -1,29 +1,33 @@
-import { View } from 'react-native';
 import styled from 'styled-components/native';
 import React from 'react';
+import { Column } from './Flex/Flex';
+import { FlexProps } from '@/components/ui/Flex/types';
+import { SIZES } from '@/constants/design-tokens';
 
-const Layout = styled(View)<{
-	variant: Props['variant']
-	$padding: Props['padding']
-}>`
-    border-radius: 8px;
-    ${({variant, $padding}) => `
-        padding: ${$padding}px;
-        border: ${variant === 'solid' ? 'solid 1px black' : 'none'};        
-        background-color: ${variant === 'solid' ? 'white' : 'none'};
-    `}
-`;
-
-type Props = {
-	padding?: number
+type Props = Pick<FlexProps, 'padding' | 'paddingHorizontal' | 'paddingVertical' | 'gap' | 'rowGap' | 'columnGap'> & {
+	width?: 'full' | 'inline'
 	variant?: 'solid' | 'ghost';
 	children?: React.ReactNode;
 }
 
-export const Card = ({variant = 'solid', padding = 12, children}: Props) => {
+const Layout = styled(Column)<{
+	variant: Props['variant']
+	$width: Props['width']
+}>`
+    min-height: ${SIZES.XXL.px};
+    border-radius: ${SIZES.XXS.px};
+    overflow: hidden;
+    ${({variant, $width}) => `
+        border: ${variant === 'solid' ? `solid ${SIZES.ONE.px} black` : 'none'};        
+        background-color: ${variant === 'solid' ? 'white' : 'none'};
+        ${$width === 'full' ? 'flexGrow: 1;' : ''}
+    `}
+`;
+
+export const Card = ({variant = 'solid', width = 'full', padding = SIZES.XS.val, children}: Props) => {
 
 	return (
-		<Layout variant={variant} $padding={padding}>
+		<Layout variant={variant} padding={padding} $width={width}>
 			{children}
 		</Layout>
 	);
